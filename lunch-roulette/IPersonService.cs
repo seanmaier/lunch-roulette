bfaae8f2bc6e527f60e079d9457eb6296ca55598
seanm <sean.maier@outlook.de>
2025-10-14 17:08:33 +0200

    feat: implement IPersonService and PersonService for managing persons

---
+++ lunch-roulette/IPersonService.cs
@@ -0,0 +1,11 @@
+﻿using lunch_roulette.models;
+
+namespace lunch_roulette;
+
+public interface IPersonService
+{
+    Person CreatePerson(Person person);
+    Person UpdatePerson(Person person);
+    bool DeletePerson(int id);
+    List<Person> GetPersons();
+}
---
+++ lunch-roulette/PersonService.cs
@@ -0,0 +1,38 @@
+﻿using lunch_roulette.models;
+
+namespace lunch_roulette;
+
+public class PersonService(AppDbContext context): IPersonService
+{
+    public Person CreatePerson(Person person)
+    {
+        context.Persons.Add(person);
+        context.SaveChanges();
+        
+        return person;
+    }
+
+    public Person UpdatePerson(Person person)
+    {
+        context.Persons.Update(person);
+        context.SaveChanges();
+        
+        return person;
+    }
+
+    public bool DeletePerson(int id)
+    {
+        var person = context.Persons.Find(id);
+        if (person == null) 
+            return false;
+        
+        context.Persons.Remove(person);
+        context.SaveChanges();
+        return true;
+    }
+
+    public List<Person> GetPersons()
+    {
+        return context.Persons.ToList();
+    }
+}
