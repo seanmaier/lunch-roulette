9e3e3668dd6d687ea20b381f77ee71c82d468368
seanm <sean.maier@outlook.de>
2025-10-14 14:02:11 +0200

    feat: add Lunch and Person models with navigation properties

---
+++ lunch-roulette/models/Lunch.cs
@@ -0,0 +1,10 @@
+﻿namespace lunch_roulette.models;
+
+public class Lunch
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    
+    // Navigation properties
+    public List<Person>? Persons { get; set; }
+}
---
+++ lunch-roulette/models/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace lunch_roulette.models;
+
+public class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Department { get; set; }
+    public string JobTitle { get; set; }
+    
+    // Navigation properties
+    public List<Lunch>? Lunches { get; set; }
+}
