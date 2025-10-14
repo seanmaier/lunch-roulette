7319c8ceecc48a0484741d6106869f31c34402a7
seanm <sean.maier@outlook.de>
2025-10-14 14:26:43 +0200

    feat: add AppDbContext with DbSet properties for Person and Lunch models

---
+++ lunch-roulette/models/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using lunch_roulette.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace lunch_roulette;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Person> Persons => Set<Person>();
+    public DbSet<Lunch> Lunches => Set<Lunch>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Person>()
+            .HasMany(p => p.Lunches)
+            .WithMany(l => l.Persons)
+            .UsingEntity(j => j.ToTable("PersonLunch"))
+            .HasIndex(p => p.Name)
+            .IsUnique();
+        
+        base.OnModelCreating(modelBuilder);
+    }
+}
