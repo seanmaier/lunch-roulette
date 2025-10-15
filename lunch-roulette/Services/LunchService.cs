791cc72c1ae726952a57485657422314c7e689a9
seanm <sean.maier@outlook.de>
2025-10-16 00:01:41 +0200

    feat: refactor LunchService for new approach of creating groups

--- lunch-roulette/Services/LunchService.cs
+++ lunch-roulette/Services/LunchService.cs
@@ -3,70 +3,65 @@ using Microsoft.EntityFrameworkCore;
 
 namespace lunch_roulette;
 
-public class LunchService(AppDbContext context): ILunchService
+public class LunchService(AppDbContext context) : ILunchService
 {
     public List<Lunch> GetLunches()
     {
         return context.Lunches
             .Include(l => l.Groups)
-            .ThenInclude(g => g.Persons)
+            .ThenInclude(g => g.Members)
+            .ThenInclude(m => m.Person)
             .OrderBy(l => l.Date)
             .ToList();
     }
 
     public Lunch CreateLunch(DateTime date)
     {
-        // Create lunch
-        // Add Group to lunch by diving people into groups of 3-4
-        // Check if persons were already in a lunch
-        // Check if Group is full
-        // Add person to group
-        var lunch = new Lunch
-        {
-            Date = date,
-        };
-        
         var persons = context.Persons.ToList();
-        var groupCount = persons.Count / 3; // Create only groups of 3 people. If modulo is not 0 add to existing group
-        
-        // Create the number of groups
-        var groups = new List<Group>();
-        for (var i = 0; i < groupCount; i++)
-        {
-            var group = new Group();
-            
-            // Add 3 persons to the group
-            // SIMPLIFIED: Needs to be updated with conditions later on
-            group.Persons.AddRange(persons.Take(3));
-            persons.RemoveRange(0, Math.Min(3, persons.Count));
-            
-            groups.Add(group);
-        }
-        
-        for (var i = 0; i < persons.Count; i++)
-        {
-            groups[i].Persons.Add(persons[i]);
-            persons.RemoveAt(i);
-        }
-        
-        // Add the remaining persons to the last group
 
-        
-        /*------------------ Just an Idea------------------*/
-        /*while (persons.Count > 0)
+        var unassigned = new List<Person>(persons); // Used to keep track of unassigned persons
+
+        var pairCounts = GetKeyPairCount(); // Dictionary for pairs
+
+        var groups = new List<List<Person>>(); // Final list of groups
+        while (unassigned.Count > 0)
         {
-            
-            
-        }*/
-        /*------------------ Just an Idea------------------*/
-        
-        lunch.Groups = groups;
-        context.Lunches.Add(lunch);
-        context.SaveChanges();
-        
-        return lunch;
+        }
+
+        var lunch = new Lunch { Date = date };
     }
-    
+
+    private Dictionary<string, int> GetKeyPairCount()
+    {
+        var pairs = new Dictionary<string, int>(); // Dictionary for pairs as keys and count as values
+
+        var allGroups = context.Groups
+            .Include(g => g.Members)
+            .ThenInclude(m => m.Person)
+            .ToList();
+
+        foreach (var group in allGroups)
+        {
+            var person = group.Members.Select(m => m.Person.Id).ToList(); // Collect just the ids to create key pairs
+
+            for (int i = 0; i < person.Count; i++)
+            {
+                for (int j = i + 1; j < person.Count; j++)
+                {
+                    var key = GetPairKey(person[i], person[j]); // Creates a key like a-b or b-a
+                    pairs[key] =
+                        pairs.GetValueOrDefault(key, 0) +
+                        1; // If the key already exists, add 1 to the count otherwise set it to 0
+                }
+            }
+        }
+
+        return pairs;
+    }
+
+    private string GetPairKey(int a, int b) =>
+        a > b ? $"{a}-{b}" : $"{b}-{a}";
+
     public bool ResetLunches()
     {
         context.Lunches.RemoveRange(context.Lunches);
